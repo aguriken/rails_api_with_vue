d59fd2972f9f914bf1d6c4202f059b3edb5f2904
中原賢一 <ohagurokenichi@yahoo.co.jp>
2020-02-18 17:52:27 +0900

    create seed data by seed_fu

---
+++ db/fixtures/01_messages.rb
@@ -0,0 +1,17 @@
+Message.seed_once(:id,
+    {
+    id:  1,
+    title: "今日の朝食",
+    content: "朝食は何も食べていません"
+    },
+    {
+    id: 2,
+    title: "今日の昼食",
+    content: "唐揚げ定食"
+    },
+    {
+    id: 3,
+    title: "今日の夕食",
+    content: "カレーかな？"
+    },
+  )
