3faa9168d4408f7c6c5c899b58bec5abd42a5526
中原賢一 <ohagurokenichi@yahoo.co.jp>
2020-02-18 17:51:48 +0900

    create messages api

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,10 @@
 module Api
   module V1
     class MessagesController < ApplicationController
+      def index
+        render json: MessageSerializer.new(Message.all).serialized_json,
+        status: :ok
+      end
     end
   end
 end
