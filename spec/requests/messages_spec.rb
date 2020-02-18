c2c99f855f03dce77ec956237d37194a28cc8031
中原賢一 <ohagurokenichi@yahoo.co.jp>
2020-02-18 15:02:15 +0900

    initial rspec setting

---
+++ spec/requests/messages_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+require 'spec_helper'
+RSpec.describe Api::V1::MessagesController, type: :request do
+  describe "GET /messages" do
+    it "works! (now write some real specs)" do
+      api_v1_messages_path
+      # expect(response).to have_http_status(200)
+    end
+  end
+end
